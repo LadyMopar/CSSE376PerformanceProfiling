8f6c1ac80d29c6dcb284021dd458789d3fdc2806
LadyMopar <richarel@rose-hulman.edu>
2015-05-05 16:35:06 -0400

    Optimized objects.Level.drawTileImage and objects.Level.draw
    used caching with a hashmap

--- src/objects/Level.java
+++ src/objects/Level.java
@@ -14,6 +14,9 @@ import java.util.HashMap;
 import javax.imageio.ImageIO;
 
 public class Level {
+
+	static HashMap<Integer, Integer> imageCache = new HashMap<Integer, Integer>();
+
 	// Tile id constants (add more here)
 	public final int DIRT = 0;
 
@@ -199,15 +202,18 @@ public class Level {
 	 *            the pixel size of a tile. 32 is perferable.
 	 */
 	BufferedImage img;
+
 	public void populateMap(String fileName, int tileSize) {
 		try {
 			String parser = " ";
 			String currentLine = null;
 			String[] currentLineValues = null;
-//			FileReader fileInput = new FileReader(fileName);
-//			BufferedReader imageReader = new BufferedReader(new FileReader(fileInput));
+			// FileReader fileInput = new FileReader(fileName);
+			// BufferedReader imageReader = new BufferedReader(new
+			// FileReader(fileInput));
 			InputStream fileInput = getClass().getResourceAsStream(fileName);
-			BufferedReader imageReader = new BufferedReader(new InputStreamReader(fileInput));
+			BufferedReader imageReader = new BufferedReader(
+					new InputStreamReader(fileInput));
 			this.mapWidth = Integer.parseInt(imageReader.readLine());
 			this.mapHeight = Integer.parseInt(imageReader.readLine());
 
@@ -227,7 +233,8 @@ public class Level {
 					this.map[c][r] = Integer.parseInt(currentLineValues[c]);
 				}
 			}
-			img = new BufferedImage(this.mapWidth*this.tileSize,this.mapHeight*this.tileSize,BufferedImage.TYPE_INT_RGB);
+			img = new BufferedImage(this.mapWidth * this.tileSize,
+					this.mapHeight * this.tileSize, BufferedImage.TYPE_INT_RGB);
 			imageReader.close();
 			generateBarrierCollisionBoxes();
 			generateRegularCollisionBoxes();
@@ -329,12 +336,21 @@ public class Level {
 	 */
 	public void draw(Graphics2D g2) {
 		int currentPosition;
-		// cache the tile background in an image so tiles don't need to be drawn again and again redundantly.
+		// cache the tile background in an image so tiles don't need to be drawn
+		// again and again redundantly.
 		Graphics2D g = img.createGraphics();
 		for (int r = 0; r < this.map.length; r++) {
 			for (int c = 0; c < this.map[r].length; c++) {
 				currentPosition = this.map[r][c];
-				drawTileImage(currentPosition, r, c, g);
+				//checking to see if currentPosition is already in the HashMap
+				if (!imageCache.containsKey(r + c)) {
+					imageCache.put(r+c, currentPosition);
+				}
+				//If the image hasn't changed, there's no point in redrawing it
+				//This checks that.
+				if(imageCache.get(r+c) != currentPosition){
+					drawTileImage(currentPosition, r, c, g);
+				}
 			}
 		}
 		g.dispose();
@@ -357,7 +373,8 @@ public class Level {
 	 *            the Graphics2D object to draw on.
 	 */
 	public void drawTileImage(int tileValue, int row, int col, Graphics2D g2) {
-		// FIXME: reduce the number of calls to the code below. When does drawImage really need to be called?
+		// FIXME: reduce the number of calls to the code below. When does
+		// drawImage really need to be called?
 		BufferedImage image = this.images.get(tileValue);
 		g2.drawImage(image, row * this.tileSize, col * this.tileSize, null);
 	}
